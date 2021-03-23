e23940de110ae1810c17e63db36cb1bb690d2dca
Reid <rlj0713@gmail.com>
2021-03-23 19:28:33 -0400

    commit tree b265f5de6e176d3afe5529e1950663d6a6fde53f

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+    get '/name' do
+        "My name is Reid Jackson"
+    end
+    get '/hometown' do
+        "My hometown is Raleigh, NC"
+    end
+    get '/favorite-song' do
+        "My favorite song is Graceland by Paul Simon"
+    end
 end
